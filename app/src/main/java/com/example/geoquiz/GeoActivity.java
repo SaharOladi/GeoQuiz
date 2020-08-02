9e7b2c0b653274b0f0162769fcbc294abe059721
SaharOladi <sahar.olady@gmail.com>
2020-08-02 11:30:38 +0430

    Adding two method for setting views in buttonTrue and buttonFalse.

--- app/src/main/java/com/example/geoquiz/GeoActivity.java
+++ app/src/main/java/com/example/geoquiz/GeoActivity.java
@@ -39,37 +39,48 @@ public class GeoActivity extends AppCompatActivity {
         mButtonTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(GeoActivity.this, R.string.toast_correct, Toast.LENGTH_SHORT);
-                // set gravity, if user choose correct, the message shows in top of layout
-                toast.setGravity(Gravity.TOP, 0 , 225);
-
-                View view_1 = toast.getView();
-                TextView textView = view_1.findViewById(android.R.id.message);
-                textView.setCompoundDrawablesWithIntrinsicBounds(0, 0,R.drawable.ic_baseline_check_24,0);
-
-                mtextView.setTextSize(20);
-                mtextView.setTextColor(Color.GREEN);
-                toast.show();
+                setViewsButtonTrue();
             }
         });
         mButtonFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast toast = Toast.makeText(GeoActivity.this, R.string.toast_incorrect, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.BOTTOM, 0, 225);
-
-                View view_1 = toast.getView();
-                TextView textView = view_1.findViewById(android.R.id.message);
-                textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_clear_24, 0, 0, 0);
-
-                mtextView.setTextSize(20);
-                mtextView.setTextColor(Color.RED);
-                toast.show();
+                setViewsButtonFalse();
             }
         });
     }
 
+    private void setViewsButtonTrue(){
+        Toast toast = Toast.makeText(GeoActivity.this, R.string.toast_correct, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0, 225);
 
+
+        View view_1 = toast.getView();
+        TextView textView = view_1.findViewById(android.R.id.message);
+        textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_baseline_check_24, 0);
+        textView.setTextSize(20);
+
+        mtextView.setTextSize(20);
+        mtextView.setTextColor(Color.GREEN);
+
+        toast.show();
+    }
+
+
+    private void setViewsButtonFalse(){
+        Toast toast = Toast.makeText(GeoActivity.this, R.string.toast_incorrect, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM, 0, 50);
+
+        View view_1 = toast.getView();
+        TextView textView = view_1.findViewById(android.R.id.message);
+        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_baseline_clear_24, 0, 0, 0);
+        textView.setTextSize(20);
+
+        mtextView.setTextSize(20);
+        mtextView.setTextColor(Color.RED);
+
+        toast.show();
+    }
     // This method find view of each views, if you don't, the initial value is null and id dose not work
     private void findViews() {
         mtextView = findViewById(R.id.textview_question_txt);
